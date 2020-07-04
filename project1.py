ca18128f2e6c4f8c3c7b8d21c93dfb642e2952bf
Khanna14Aman <67836775+Khanna14Aman@users.noreply.github.com>
2020-07-04 22:46:23 +0530

    Add files via upload
    Code to find and print area of circle
    
    Code to print extension of file

---
+++ project1.py
@@ -0,0 +1,25 @@
+a = input("enter radius of circle to find area : ")
+area=(3.14*a*a)
+print("area of circle is :" )
+print(area)
+
+
+
+
+
+x = input("enter any filename as string and using extension ")
+
+if ".py" in x:
+    print("The extension of file is :  .python")
+else :
+    if ".c" in x:
+        print("The extension of file is :  .c")
+    else :
+        if ".cpp" in x:
+            print("The extension of file is :  .c++")
+        else :
+            print("please use any extension ")
+   
+
+
+      
